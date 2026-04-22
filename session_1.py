8ed558acafc00acd0f488d59fa4499f77749b680
Nerraw-arch <lemeinwarren@gmail.com>
2026-04-22 18:24:30 +0300

    session 2 (variables and arithmetic operators

--- session_1.py
+++ session_1.py
@@ -1,1 +1,84 @@
 print("Hello world")
+
+print("Welcome to python programming")
+
+#first variable
+
+age = 23
+
+car_brand = "Toyota"
+
+university = "University of Nairobi"
+
+AGE = 24
+Age = 25
+
+#The 3 ages(line 7,13&14) are different because of the case sensitivity of python
+
+print(car_brand)
+print(university)
+
+#Arithmetic operators
+
+#Addition operator is the symbol + in python
+
+no_1 = 10
+no_2 = 21
+no_3 = "30"
+no_4 = 40
+
+first_summation = no_1 + no_2
+print(first_summation)
+
+print(no_1 + no_2 )
+
+#print(no_3 + no_2)
+print(type(no_3))
+print(type(university)) #type is used to check the data type of the variable
+
+no_5 = 10.5
+print(type(no_1))
+print(type(no_5))
+datatype_bool = True
+
+print(type(datatype_bool))
+
+#subraction
+
+#minus in python is represe`nted by the symbol -`
+
+print(no_2 - no_1)
+
+print(no_5 - no_1)
+
+#multiplication operator is represented by the symbol *
+
+print(no_1 * no_2)
+print(no_1 * no_5)
+
+#division operator is represented by the symbol /
+ 
+print(no_2 / no_1)
+print(no_5 / no_1)
+print(no_4 / no_1)
+print(no_5 / no_4)
+print(no_4 / no_5)
+
+#floor division operator is represented by the symbol //
+
+print(no_4 // no_5)
+print(no_5 // no_4)
+
+#modulus operator is represented by the symbol %
+
+print(no_4 % no_5)
+print(no_5 % no_4)
+print(7 / 2)
+print(7 // 2)
+print(7 % 2)
+
+#exponentiation operator is represented by the symbol **
+print(10**2)
+print(2**10)
+print(100 ** 0.5)
+print(100 ** (1/3))
